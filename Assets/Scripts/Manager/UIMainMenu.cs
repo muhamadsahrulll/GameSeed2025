863878807a79ca90b08da6b8a54becf2c765bde3
Rizky Syahputra Setyawan <rizkysyahputra12347890@gmail.com>
2025-08-04 04:45:19 +0700

    fix game

--- Assets/Scripts/Manager/UIMainMenu.cs
+++ Assets/Scripts/Manager/UIMainMenu.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class UIMainMenu : MonoBehaviour
 {
+    public int C = 0;
+    public List<Sprite> tutorialSpr;
+    public Image tutorialContainer;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,24 @@ public class UIMainMenu : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void InteractTutorialNext()
+    {
+        if(tutorialSpr != null & C < tutorialSpr.Count - 1)
+        {
+            C++;
+            tutorialContainer.sprite = tutorialSpr[C];
+        }
+    }
+    
+    public void InteractTutorialPrevious()
+    {
+        if(tutorialSpr != null & C > 0)
+        {
+            C--;
+            tutorialContainer.sprite = tutorialSpr[C];
+        }
     }
 
     public void InteractMusicButton()
